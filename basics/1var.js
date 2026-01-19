95b3b66ba8620f709c2a64c147192c38b85e3018
Rishabh Rajput <rishabhrajput04576@gmail.com>
2026-01-19 12:08:45 +0000

    datatypes

---
+++ basics/1var.js
@@ -0,0 +1,19 @@
+const accountId = 2222
+let accountEmail = "risusjujhs@gmail.com"
+var accountPassword =  "iu943r34h"
+accountCity = "jaipureeeeeeeeeeeeeeee"
+let accountState;
+let accountMember = ("")
+console.log(accountEmail , accountId , accountPassword , accountCity);
+
+
+    // // accountId = 8888 not allow
+    // console.log(accountId);
+
+    // prefer not to use var 
+     because of issue in block scope and functional scope */
+   
+ accountEmail = "rijhs@gmail.com"
+ accountPassword =  "xxxxxxxxxxx"
+accountCity = "kakore"
+ console.table([accountEmail,accountId,accountPassword,accountCity,accountState,accountMember])
---
+++ basics/2dt.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3 + 3)  //we are using node.js not browser
+
+
+console.log( 3+3)
+
+console.log("rishabh")
+
+
+
+let name = "rishabh"
+let age = "22"
+ let islogin = false
+ let state;
+ let country= "33333"
+
+
+ //number => 2 to power 53
+ //bigint
+ //string => ""
+ // boolean => true/false
+ // null => stande alone value
+ // undefine => 
+  // symbol => unique
+ 
+ 
+
+ // object 
+
+console.log(typeof"rishabh" , age , islogin, state, country )
+console.log(typeof age);
+console.log(typeof islogin);
+console.log(typeof state)
+console.log(typeof country)
+console.log(typeof null)// object
+console.log(typeof undefined) //undefine

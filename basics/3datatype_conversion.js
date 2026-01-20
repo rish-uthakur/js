2dbf423f04c433d47cf7e7fea625240640a79b6f
Rishabh Rajput <rishabhrajput04576@gmail.com>
2026-01-20 05:38:29 +0000

    conversion of datatype

---
+++ basics/3datatype_conversion.js
@@ -0,0 +1,66 @@
+let score = "44xyz"
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber = Number (score)
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber)
+
+
+
+ let scoree = null
+
+console.log(typeof scoree)
+console.log(typeof(scoree));
+
+let valueInNumberr = Number (scoree)
+ console.log(typeof valueInNumberr)
+ console.log(valueInNumberr)
+
+
+
+ let scoreee = true
+
+console.log(typeof scoreee)
+console.log(typeof(scoreee));
+
+let valueInNumberrr = Number (scoreee)
+ console.log(typeof valueInNumberrr)
+ console.log(valueInNumberrr)
+
+
+ let scoreeee = undefined
+
+console.log(typeof scoreeee)
+console.log(typeof(scoreeee));
+
+let valueInNumberrrr = Number (scoreeee)
+ console.log(typeof valueInNumberrrr)
+ console.log(valueInNumberrrr)
+
+
+  let scoreeeee = "rishabh"
+
+console.log(typeof scoreeeee)
+console.log(typeof(scoreeeee));
+
+let valueInNumberrrrr = Number (scoreeeee)
+ console.log(typeof valueInNumberrrrr)
+ console.log(valueInNumberrrrr)
+
+
+ // "44" => 44
+ // "44xyz" => NaN
+ // true => 1 ; false => 0;
+
+
+ let isLoggedIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn)
+ console.log(typeof booleanIsLoggedIn)
+
+ // 1 => true; 0 => false
+ // "" => false;
+ // "rishabh" = true;

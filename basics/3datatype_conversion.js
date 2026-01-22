320b030832c4df709b45cd0234de2ea984ecdab4
Rishabh Rajput <rishabhrajput04576@gmail.com>
2026-01-22 15:20:36 +0000

    operations

--- basics/3datatype_conversion.js
+++ basics/3datatype_conversion.js
@@ -1,53 +1,53 @@
 let score = "44xyz"
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
 let valueInNumber = Number (score)
- console.log(typeof valueInNumber)
- console.log(valueInNumber)
+//  console.log(typeof valueInNumber)
+//  console.log(valueInNumber)
 
 
 
  let scoree = null
 
-console.log(typeof scoree)
-console.log(typeof(scoree));
+// console.log(typeof scoree)
+// console.log(typeof(scoree));
 
 let valueInNumberr = Number (scoree)
- console.log(typeof valueInNumberr)
- console.log(valueInNumberr)
+//  console.log(typeof valueInNumberr)
+//  console.log(valueInNumberr)
 
 
 
  let scoreee = true
 
-console.log(typeof scoreee)
-console.log(typeof(scoreee));
+// console.log(typeof scoreee)
+// console.log(typeof(scoreee));
 
 let valueInNumberrr = Number (scoreee)
- console.log(typeof valueInNumberrr)
- console.log(valueInNumberrr)
+//  console.log(typeof valueInNumberrr)
+//  console.log(valueInNumberrr)
 
 
  let scoreeee = undefined
 
-console.log(typeof scoreeee)
-console.log(typeof(scoreeee));
+// console.log(typeof scoreeee)
+// console.log(typeof(scoreeee));
 
 let valueInNumberrrr = Number (scoreeee)
- console.log(typeof valueInNumberrrr)
- console.log(valueInNumberrrr)
+//  console.log(typeof valueInNumberrrr)
+//  console.log(valueInNumberrrr)
 
 
   let scoreeeee = "rishabh"
 
-console.log(typeof scoreeeee)
-console.log(typeof(scoreeeee));
+// console.log(typeof scoreeeee)
+// console.log(typeof(scoreeeee));
 
 let valueInNumberrrrr = Number (scoreeeee)
- console.log(typeof valueInNumberrrrr)
- console.log(valueInNumberrrrr)
+//  console.log(typeof valueInNumberrrrr)
+//  console.log(valueInNumberrrrr)
 
 
  // "44" => 44
@@ -58,9 +58,49 @@ let valueInNumberrrrr = Number (scoreeeee)
  let isLoggedIn = 1
 
  let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn)
- console.log(typeof booleanIsLoggedIn)
+//  console.log(booleanIsLoggedIn)
+//  console.log(typeof booleanIsLoggedIn)
 
  // 1 => true; 0 => false
  // "" => false;
  // "rishabh" = true;
+
+
+
+
+  // **************************** Oprations **************************************
+
+
+  let value = 3 
+  let negvalue = -value 
+  // console.log(negvalue);
+  
+
+  // console.log(2+2)
+  // console.log(2-2)
+  // console.log(2*2)
+  // console.log(2**2)
+  // console.log(2/2)
+  // console.log(5%2)
+
+
+  let str1 = "rishabh"
+
+  let str2 = " Hello"
+  let str3 = str1  +  str2
+
+  // console.log(str3)
+
+
+  // console.log("4" + 6 )
+  // console.log( 4 + "6" )
+  // console.log("4" + 6  + 6)
+  // console.log( 4 + 6  + "6")
+  // console.log("4" + 6  - 6)
+  // console.log("4" - 6  - 6)
+  // console.log("4" + 6  - 6 * 5 / 2 %3)
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+

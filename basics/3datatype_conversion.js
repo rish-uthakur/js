17c054e7b705a9653e0466346bca0e9a0b9394ca
Rishabh Rajput <rishabhrajput04576@gmail.com>
2026-01-26 15:34:29 +0000

    comparision

--- basics/3datatype_conversion.js
+++ basics/3datatype_conversion.js
@@ -104,3 +104,6 @@ let valueInNumberrrrr = Number (scoreeeee)
 // console.log(+true)
 // console.log(+"")
 
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1 , num2 , num3)
---
+++ basics/4comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1)
+// console.log(2>=1) 
+ 
+// console.log(2- 1) 
+// console.log(2>true) 
+
+
+// console.log("3">1)
+// console.log("03">1)
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null > undefined );
+
+// comparision or equality check both are diffrent
+
+
+// ******************************   === strickly  check  *****************************
+
+// ===// in === (stric checks) checks datatype and value
+
+console.log("2"===2)
